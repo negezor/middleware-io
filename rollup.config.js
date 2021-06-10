00ec90173d676b9613792cd59fe6d1e5c4dc136f
Negezor <negezor@gmail.com>
2021-06-11 00:48:02 +1100

    feat(module): use exports in package.json

--- rollup.config.js
+++ rollup.config.js
@@ -39,12 +39,12 @@ export default [
 		],
 		output: [
 			{
-				file: `${pkg.main}.js`,
+				file: pathJoin(lib, 'index.js'),
 				format: 'cjs',
 				exports: 'named'
 			},
 			{
-				file: `${pkg.main}.mjs`,
+				file: pathJoin(lib, 'index.mjs'),
 				format: 'esm'
 			}
 		]
